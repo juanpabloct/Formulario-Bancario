ddc7c514d091c8ffd466e1edb0f72e785f48ce78
Juan Pablo Castellanos Torres <juanptorres2003@gmail.com>
2022-06-23 20:22:57 -0500

    se creo por aparte un diccionario de los valores que va a tener el formulario

---
+++ src/variables/camposInputs.js
@@ -0,0 +1,14 @@
+const camposInputs = {
+  name: "",
+  lastName: "",
+  Age: "",
+  "Money month": "",
+  "Number phone": "",
+  "Fixed number": "",
+  estate: "",
+  description: "",
+  date: "",
+  work: "",
+  image: "",
+};
+export { camposInputs };
